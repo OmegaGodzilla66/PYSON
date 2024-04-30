dd40aaabc325cda8f489e5da9c546d2cf0d3c3b3
CoolSchnoodle <142824566+CoolSchnoodle@users.noreply.github.com>
2024-04-30 17:03:40 +0000

    Make tests decent

--- example.py
+++ example.py
@@ -1,4 +1,5 @@
 import pyson # to use this in your project, simply change the name from "pyson" to the name of the file you're importing it from
+import os
 
 # Read test
 print("-----READ TEST-------")
@@ -11,20 +12,26 @@ print(pyson.getData("example.pyson","float_example"))
 print("---WHOLE TEST--------")
 print(pyson.getWhole("example.pyson"))
 
+# Write test
+print("---WRITE TEST--------")
+pyson.write("newfile.pyson", "string", "str", "value", "w")
+
 # Append test
 print("---APPEND TEST-------")
-try:
-    pyson.write("example.pyson","write_example","list",["this","is","a","new","list"])
-except Exception:
-    # stupid legacy codebase
-    print("lol duplicate values present")
-print(pyson.getData("example.pyson","write_example"))
+pyson.write("newfile.pyson", "integer", "int", 123, "a")
+pyson.write("newfile.pyson", "floating-point", "float", 3.14, "a")
+pyson.write("newfile.pyson", "string list", "string", ["this is a list", "of strings"])
+print(pyson.getWhole("newfile.pyson"))
 
 # Compatability test
 print("---COMPATABILITY TEST------")
 print(pyson.checkCompatible("example.pyson"))
+print(pyson.checkCompatible("newfile.pyson"))
+os.remove("newfile.pyson")
 
 # update val test
 print("---UPDATING TEST---")
-print(pyson.updateData("example.pyson", "str_example", "this is an updated string"))
+pyson.updateData("example.pyson", "str_example", "this is an updated string")
+print(pyson.getData("example.pyson", "str_example"))
+pyson.updateData("example.pyson", "str_example", "this is a string")
 print(pyson.getData("example.pyson", "str_example"))
