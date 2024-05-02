51cc0f67c89b3316d96b023bd0c22bd0c4fe1b77
ComputingSquid <jwbolling001@gmail.com>
2024-05-01 19:11:23 -0500

    added package to pypi

--- tests/example.py
+++ tests/example.py
@@ -1,11 +1,11 @@
-import pyson # to use this in your project, simply change the name from "pyson" to the name of the file you're importing it from
+from src.pyson_data import pyson
 
 # Read test
 print("-----READ TEST-------")
-print(pyson.getData("example.pyson","str_example"))
-print(pyson.getData("example.pyson","int_example"))
-print(pyson.getData("example.pyson","list_example"))
-print(pyson.getData("example.pyson","float_example"))
+print(pyson.getData("example.pyson", "str_example"))
+print(pyson.getData("example.pyson", "int_example"))
+print(pyson.getData("example.pyson", "list_example"))
+print(pyson.getData("example.pyson", "float_example"))
 
 # Whole test
 print("---WHOLE TEST--------")
@@ -14,11 +14,11 @@ print(pyson.getWhole("example.pyson"))
 # Append test
 print("---APPEND TEST-------")
 try:
-    pyson.write("example.pyson","write_example","list",["this","is","a","new","list"])
+    pyson.write("example.pyson", "write_example", "list", ["this", "is", "a", "new", "list"])
 except Exception:
     # stupid legacy codebase
     print("lol duplicate values present")
-print(pyson.getData("example.pyson","write_example"))
+print(pyson.getData("example.pyson", "write_example"))
 
 # Compatability test
 print("---COMPATABILITY TEST------")
