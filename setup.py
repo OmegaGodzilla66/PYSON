cfd68eb9868147a45ac10a442d514bfadb07dae6
Jacob <66024281+ProbablyComputingSquid@users.noreply.github.com>
2024-05-06 12:11:07 -0500

    setup fix?

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-
+# TODO: fix this
 setuptools.setup(
-     scripts=['src/pyson_data'],
+     # this line doesn't work as intended
+     #scripts=['src/pyson_data'],
      author="josh-co-dev",
      description="Provides support for storing data in .pyson format",
      long_description=long_description,
