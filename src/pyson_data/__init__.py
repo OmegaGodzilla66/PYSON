5cabea1f70371b7404d9258a53bb5421fe505d47
Jacob <66024281+ProbablyComputingSquid@users.noreply.github.com>
2024-05-09 18:36:38 -0500

    __init__.py update for new functions
    change to star import
    
    happy now?

--- src/pyson_data/__init__.py
+++ src/pyson_data/__init__.py
@@ -1,6 +1,6 @@
-from .pyson import getData, getWhole, write, writeMultiple, updateData, checkCompatible
-
-
+#from .pyson import getData, getList, write, writeMultiple, updateData, checkCompatible, getDict
+from .pyson import *
+# are you happy now, Eli?
 def hello():
     print("hello world")
     print("I love testing in production")
