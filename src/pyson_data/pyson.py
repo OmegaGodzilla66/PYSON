564eb6fc128f9d4433c744378c434b3504657e0f
CoolSchnoodle <142824566+CoolSchnoodle@users.noreply.github.com>
2024-05-28 19:38:58 +0000

    Add a Value class to represent a pyson value

--- src/pyson_data/pyson.py
+++ src/pyson_data/pyson.py
@@ -4,7 +4,7 @@ class Type:
         Initialize a new pyson Type
         Takes 1 argument, which must be either "int", "float", "str", or "list"
         """
-        if type not in ["int", "float", "str", list]:
+        if type not in ["int", "float", "str", "list"]:
             raise ValueError(f"Invalid type {type}")
         self.type: str = type
 
@@ -26,4 +26,44 @@ class Type:
         """
         Reduce the Type object so it can be pickled
         """
-        return (Type, (self.type,))
+        return (self.__class__, (self.type,))
+
+class Value:
+    def __init__(self, value: int | float | str | list[str]) -> None:
+        """
+        Initialize a new pyson Value
+        Takes 1 argument, which must be either an int, float, str, or list of str
+        """
+        self.value: int | float | str | list[str] = value
+        if isinstance(value, int):
+            self.type = Type("int")
+        elif isinstance(value, float):
+            self.type = Type("float")
+        elif isinstance(value, str):
+            self.type = Type("str")
+        elif isinstance(value, list):
+            if any(not isinstance(item, str) for item in value):
+                raise ValueError("Lists in pyson must contain only strings")
+            self.type = Type("list")
+        else:
+            raise ValueError(f"Invalid pyson type type {type(value)}")
+
+    def __str__(self) -> str:
+        """
+        String representation of the Value
+        """
+        return f"{self.type}:{self.value}"
+
+    def __repr__(self) -> str:
+        """
+        String representation of the Value
+        Unlike Value.__str__(), this method will include the fact that
+        this is a Value object from the pyson_data package
+        """
+        return f"pyson_data.Value(type = {self.type}, value = {self.value})"
+
+    def __reduce__(self) -> tuple:
+        """
+        Reduce the Value object so it can be pickled
+        """
+        return (self.__class__, (self.value, self.type))
